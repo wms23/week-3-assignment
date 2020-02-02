ebac6ccd89570d67ddd0d43e531c4f9e4ba52698
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2020-02-02 11:45:00 +0630

    Vue component complete

---
+++ resources/js/components/TextInput.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="form-group">
+    <label :for="name"><slot></slot></label>
+    <input type="text" class="form-control" :id="name" value :name="name" :placeholder="placeholder" />
+  </div>
+</template>
+<script>
+export default {
+    props : ['name','placeholder']
+};
+</script>
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -4,10 +4,11 @@
     <div>
         <form action="{{route('post.store')}}" method="POST">
             @csrf
-            <div class="form-group">
-                <label for="title">Title</label>
+            <text-input name="title" placeholder="Enter Title">Title</text-input>
+            <!-- <div class="form-group">
+                <label for="title"></label>
                 <input type="text" class="form-control" id="title" value="" name="title" placeholder="Enter title">
-            </div>
+            </div> -->
             <div class="form-group">
                 <label for="content">Content</label>
                 <textarea class="form-control" id="content" name="content" rows="6"></textarea>
